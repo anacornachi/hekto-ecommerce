a266b8104e1ee7b1a2ee5a5ea4868329e5ceb7d2
anacornachi <anacorn4chi@gmail.com>
2022-01-28 16:44:18 -0300

    header component with completed responsiveness

---
+++ src/components/Header/navigation.ts
@@ -0,0 +1,22 @@
+export const navigation = [
+  {
+    link: "/",
+    label: "Home",
+  },
+  {
+    link: "/products",
+    label: "Products",
+  },
+  {
+    link: "/blog",
+    label: "Blog",
+  },
+  {
+    link: "/shop",
+    label: "Shop",
+  },
+  {
+    link: "/contact",
+    label: "Contact",
+  },
+];
