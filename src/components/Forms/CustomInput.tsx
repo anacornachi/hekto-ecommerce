204aeb8b011fb164fa7b18d70c62fa7148baad27
anacornachi <anacorn4chi@gmail.com>
2022-02-16 14:45:27 -0300

    create an account page and add element in button when type is password

--- src/components/Forms/CustomInput.tsx
+++ src/components/Forms/CustomInput.tsx
@@ -1,11 +1,15 @@
 import {
+  Button,
   FormControl,
   FormErrorMessage,
   Input,
   InputGroup,
   InputProps,
+  InputRightElement,
 } from '@chakra-ui/react';
+import {useState} from 'react';
 import {useFormContext} from 'react-hook-form';
+import {IoEyeOffOutline, IoEyeOutline} from 'react-icons/io5';
 
 export type Props = {
   name: string;
@@ -19,6 +23,8 @@ export default function CustomInput({
   variant,
   ...props
 }: Props) {
+  const [showPassword, setShowPassword] = useState(false);
+
   const {formState, register} = useFormContext();
 
   const {errors} = formState;
@@ -29,10 +35,40 @@ export default function CustomInput({
       <InputGroup>
         <Input
           {...props}
+          type={
+            props.type === 'password'
+              ? showPassword
+                ? 'text'
+                : 'password'
+              : undefined
+          }
+          px="16px"
+          pr={props.type === 'password' ? '15%' : '16px'}
           variant={variant ?? 'outlineInput'}
           {...register(name)}
         />
       </InputGroup>
+      {props.type === 'password' ? (
+        <InputRightElement>
+          <Button
+            h="30px"
+            w="100%"
+            mt="25px"
+            size="lg"
+            bg="none"
+            p="0"
+            _focus={{boxShadow: 'none', bg: 'none'}}
+            _hover={{bg: 'none'}}
+            _active={{bg: 'none'}}
+            onMouseDown={() => setShowPassword(!showPassword)}
+            onMouseUp={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
+          </Button>
+        </InputRightElement>
+      ) : (
+        <></>
+      )}
       <FormErrorMessage
         fontSize={{base: 'xs', lg: 'sm'}}
         mt="4px"
---
+++ src/pages/account/sign-up.tsx
@@ -0,0 +1,11 @@
+import SignUp from '@components/Forms/SignUp';
+import SubHeader from '@components/SubHeader';
+
+export default function CreateAccount() {
+  return (
+    <>
+      <SubHeader title="Sign Up" />
+      <SignUp />
+    </>
+  );
+}
