abfd7da27efc57f24273d31e672a75fc24a1bcaa
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-02-27 16:26:21 -0300

    tests setup

--- __tests__/login.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import Login from 'src/pages/account/login';
-
-test('submit login form', () => {
-  // render(<Login />);
-  // render(<SubHeader title="My Account" />);
-  // const button = screen.getByRole('button', {name: 'Teste'});
-  // const checkbox = screen.getByRole('checkbox');
-});
---
+++ src/components/Forms/__tests__/login.test.tsx
@@ -0,0 +1,6 @@
+import {render, screen} from '@testing-library/react';
+import Login from '../Login';
+
+test('submit login form', () => {
+  render(<Login />);
+});
